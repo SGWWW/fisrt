ab62a6902eafdadd7ae03404b1e3c76dca17d6f9
SGWWW <40271379+SGWWW@users.noreply.github.com>
2021-06-05 22:15:22 +0800

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,8 +1,8 @@
 import yagmail
 #链接邮箱服务器
-yag=yagmail.SMTP(user="Gry1417@163.com",password="shi123...",host="smtp.163.com")
+yag=yagmail.SMTP("2369282540@qq.com",password="xgukgqroswdzeaic",host="smtp.163.com",465)
 #邮箱正文
 contents="打卡成功"
 subject="打卡成功"
 #发送邮件
-yag.send=('2369282540@qq.com',subject,contents)
+yag.send=('Gry1417@163.com',subject,contents)
